b7d20ded1c3685f035b32619012d89ecbe85129a
ovi2410 <guneshwarifegade@gmail.com>
2022-07-25 23:56:41 +0530

    [ovi]Add:create database Added

---
+++ AddressBookADO.NET/AddressBookRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookADO.NET
+{
+    public class AddressBookRepo
+    { //Give path for Database Connection
+        public static string connection = @"Server=.;Database=AddressBookServices;Trusted_Connection=True;";
+        //Represents a connection to Sql Server Database
+        SqlConnection sqlConnection = new SqlConnection(connection);
+    }
+
+
+}
+
---
+++ AddressBookADO.NET/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace AddressBookADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome TO Address Book System Using ADO.NEt AN TSql");
+        }
+    }
+}
